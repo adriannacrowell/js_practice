5ee05e821a867cbceab645cb86fb4ef7cab9a659
amarie004 <amarie004@gmail.com>
2016-03-18 13:05:01 -0400

    cleaned code with eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,11 @@
 module.exports = function(grunt) {
 		// All of your Grunt code must be specified inside this function!
 		// Setup configuration...
-    // Load tasks...
-    // Define tasks...
+		// Load tasks...
+		// Define tasks...
 
-    grunt.initConfig({
-			pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 /*			jshint: {
 					scripts: {
 							src: ['scripts/**.js', 'lib/**.js']
@@ -23,95 +23,92 @@ module.exports = function(grunt) {
 */
 
 		eslint: {
-				options: {
-						configFile: 'conf/eslint.json',
-						rulePaths: ['conf/rules']
-				},
-				target: ['file.js']
+			all: ['**/*.js']
 		},
 
-			uglify: {
-				options: {
-					banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-				},
-				build: {
-					src: 'src/<%= pkg.name %>.js',
-					dest: 'build/<%= pkg.name %>.min.js'
-				},
-					scripts: {
-							expand: true,
-							cwd: 'scripts/',
-							src: '**.js',
-							dest: 'build/',
-							ext: '.min.js'
-					}
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			},
+			scripts: {
+				expand: true,
+				cwd: 'scripts/',
+				src: '**.js',
+				dest: 'build/',
+				ext: '.min.js'
+			}
+		},
 
-			babel: {
-					 options: {
-							 sourceMap: true,
-							 stage: 0,
-							 optional: ['runtime'],
-					 		 presets: ['babel-preset-es2015']
-					 },
-					 dist: {
-							 files: [
-								 // Node source
-								 {
-								 expand: true,// Enable dynamic expansion.
-								 cwd: 'js_practice/',  // Src matches are relative to this path.
-								 src: ['es6/*.js'],
-								 dest: 'dist/-<%= grunt.template.today("yyyy-mm-dd") %>.js', // Destination path prefix. File will be copied to the dist directory with today’s data in the filename.
-							   ext: '.js',   // Dest filepaths will have this extension.
-						 },
-
-								 // Browser source
-								{
-									expand: true,// Enable dynamic expansion.
-									cwd: 'js_practice/',  // Src matches are relative to this path.
-									src: ['public/es6/*.js'],
-									dest: 'public/dist/-<%= grunt.template.today("yyyy-mm-dd") %>.js', // Destination path prefix. File will be copied to the dist directory with today’s data in the filename.
-									ext: '.js',   // Dest filepaths will have this extension.
-								},
-						],
+		babel: {
+			options: {
+				sourceMap: true,
+				stage: 0,
+				optional: ['runtime'],
+				presets: ['babel-preset-es2015']
+			},
+			dist: {
+				files: [
+								// Node source
+					{
+						expand: true,// Enable dynamic expansion.
+						cwd: 'js_practice/',	// Src matches are relative to this path.
+						src: ['es6/*.js'],
+						dest: 'dist/-<%= grunt.template.today("yyyy-mm-dd") %>.js', // Destination path prefix. File will be copied to the dist directory with today’s data in the filename.
+						ext: '.js'	 // Dest filepaths will have this extension.
 					},
-			 },
+
+								// Browser source
+					{
+						expand: true,// Enable dynamic expansion.
+						cwd: 'js_practice/',	// Src matches are relative to this path.
+						src: ['public/es6/*.js'],
+						dest: 'public/dist/-<%= grunt.template.today("yyyy-mm-dd") %>.js', // Destination path prefix. File will be copied to the dist directory with today’s data in the filename.
+						ext: '.js'	 // Dest filepaths will have this extension.
+					}
+				]
+			}
+		},
 /*
 			browserify: {
-	       dist: {
-	         options: {
-	           transform: [["babelify", { "stage": 0 }]]
-	         },
-	         files: {
-	           "build/bundle.js": "src/main.js"
-	         }
-	       }
+				 dist: {
+					 options: {
+						 transform: [["babelify", { "stage": 0 }]]
+					 },
+					 files: {
+						 "build/bundle.js": "src/main.js"
+					 }
+				 }
 			 },
 */
 
-        watch: {
-					scripts: {
-				    files: ['**/*.js'],
-				    tasks: ['jshint'],
-						},
+		watch: {
+			scripts: {
+				files: ['**/*.js'],
+				tasks: ['jshint']
+			},
 
-            styles: {
-                files: 'styles/**.less',
-                task: 'less:styles'
-            }
-        }
+			styles: {
+				files: 'styles/**.less',
+				task: 'less:styles'
+			}
+		}
 
-    });
+	});
 
-  /*  grunt.loadNpmTasks('grunt-contrib-jshint');*/
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-	/*	grunt.loadNpmTasks('grunt-browserify');*/
-		grunt.loadNpmTasks('grunt-babel');
+	/*	grunt.loadNpmTasks('grunt-contrib-jshint');*/
+	grunt.loadNpmTasks('grunt-contrib-eslint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+/*	grunt.loadNpmTasks('grunt-browserify');*/
+	grunt.loadNpmTasks('grunt-babel');
 
-  // Default task.
-    grunt.registerTask('default', ['babel']);
-    grunt.registerTask('build', [ 'uglify']);
+// Default task.
+	grunt.registerTask('default', ['eslint', 'babel']);
+	grunt.registerTask('build', [ 'uglify']);
 
 };
